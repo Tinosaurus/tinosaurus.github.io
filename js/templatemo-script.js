059e6f5237bb098ce365906c6fa4b3fc719a92f6
Tino Roivanen <tino.roivanen98@gmail.com>
2020-08-31 15:15:51 +0300

    Initial version of the webpage
    Initial push.

---
+++ js/templatemo-script.js
@@ -0,0 +1,51 @@
+const initBg = (autoplay = true) => {
+    const bgImgName = ['bg.jpg'];
+    const bgImgs = bgImgName.map(img => "img/" + img);
+
+    $.backstretch(bgImgs, {duration: 0, fade: 500});  
+}
+
+const setBg = id => {
+    $.backstretch('show', id);
+}
+
+const setBgOverlay = () => {
+    const windowWidth = window.innerWidth;
+    const bgHeight = $('body').height();
+    const tmBgLeft = $('.tm-bg-left');
+
+    $('.tm-bg').height(bgHeight);
+
+    if(windowWidth > 768) {
+        tmBgLeft.css('border-left', `0`)
+                .css('border-top', `${bgHeight}px solid transparent`);                
+    } else {
+        tmBgLeft.css('border-left', `${windowWidth}px solid transparent`)
+                .css('border-top', `0`);
+    }
+}
+
+$(document).ready(function () {
+    const autoplayBg = false;	// set Auto Play for Background Images
+    initBg(autoplayBg);    
+    setBgOverlay();
+
+    const bgControl = $('.tm-bg-control');            
+    bgControl.click(function() {
+        bgControl.removeClass('active');
+        $(this).addClass('active');
+        const id = $(this).data('id');                
+        setBg(id);
+    });
+
+    $(window).on("backstretch.after", function (e, instance, index) {        
+        const bgControl = $('.tm-bg-control');
+        bgControl.removeClass('active');
+        const current = $(".tm-bg-controls-wrapper").find(`[data-id=${index}]`);        
+        current.addClass('active');
+    });
+
+    $(window).resize(function() {
+        setBgOverlay();
+    });
+});
